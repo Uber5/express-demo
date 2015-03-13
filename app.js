19b628566b4707dbe5ce51453209d7657d50d2d3
Thabang Tseboho <tseboho@gmail.com>
2015-03-13 16:07:42 +0200

    Added date joined for profiles.

--- app.js
+++ app.js
@@ -5,75 +5,13 @@ var app = express();
 app.set('view engine', 'ejs');
 
 // MIDDLEWARE
-// TODO: Thabang: Find out reason for 'extended: true/false' option.
+// TODO: Thabang: Find out purpose for 'extended: true/false' option.
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({
 	extended: true,
 }));
 
-// VIEWS
-app.get('/', function (req, res) {
-  res.render('pages/index', {title: "Hi there, I'm an Express Server! Enter your details below."});
-});
-
-// APIS
-function findProfileByID(id){
-	var response = null;
-	profiles.forEach(function(profile){
-		if (profile.id == id) {
-			response = profile;
-		}
-	});
-	return response;
-}
-
-function findProfileByEmail(email){
-	var response = null;
-	profiles.forEach(function(profile){
-		if (profile.email == email) {
-			response = profile;
-		}
-	});
-	return response;
-}
-
-app.get('/api/profiles', function(req, res){
-	res.status(200).send(profiles);
-});
-
-app.get('/api/profiles/:id', function(req, res){
-	var response = findProfileByID(req.params.id);
-	
-	if ( response == null ){
-			res.status(404).send("Profile Was not Found!");
-		} else {
-			res.status(200).send(response);
-		};
-});
-
-app.post('/api/profiles', function(req, res){
-	var profile = req.body;
-	var response = findProfileByEmail(profile.email);
-
-	if (response == null) {
-		lastID++;
-		profile.id = lastID;
-		profiles.push(profile);	
-		res.status(201).send("Status: " + res.statusCode + " Added new user with id " + profile.id);
-	} else {
-		res.status(200);
-		res.send("Status: " +res.statusCode+ " Profile with email: " + profile.email + " already exists!");
-	};
-});
-
-// app.put('api/profiles', function(req, res){
-// 	// Update profile using email as identifier.
-// });
-
-// app.delete('api/profiles', function(req, res){
-// 	// Delete profile using email as identifier.
-// });
 
 // Dummy Database.
 var profiles = [];
@@ -93,13 +31,84 @@ var fixtures = [
 ];
 
 fixtures.forEach(function(value){
+	var now = new Date().toJSON();
 	lastID++;
+
+	value.date_joined = now;
 	value.id = lastID;
 	profiles.push(value);
 });
 
-// SERVER
 
+// VIEWS
+app.get('/', function (req, res) {
+  res.render('pages/index', {title: "Hi there, I'm an Express Server! Enter your details below."});
+});
+
+
+// APIS
+// Find Profile from dummy DB with ID. Returns profile or null.
+// TODO: OPTIONAL: Thabang: Find better exhaustive search method.
+function findProfileByID(id){
+	var response = null;
+	profiles.forEach(function(profile){
+		if (profile.id == id) {
+			response = profile;
+		}
+	});
+	return response;
+}
+
+function findProfileByEmail(email){
+	var response = null;
+	profiles.forEach(function(profile){
+		if (profile.email == email) {
+			response = profile;
+		}
+	});
+	return response;
+}
+// Profiles GET ALL
+app.get('/api/profiles', function(req, res){
+	res.status(200).send(profiles);
+});
+// Profiles GET by ID
+app.get('/api/profiles/:id', function(req, res){
+	var response = findProfileByID(req.params.id);
+	
+	if ( response == null ){
+			res.status(404).send("Profile Was not Found!");
+		} else {
+			res.status(200).send(response);
+		};
+});
+// Profiles POST
+app.post('/api/profiles', function(req, res){
+	var profile = req.body;
+	var response = findProfileByEmail(profile.email);
+
+	if (response == null) {
+		var now = new Date().toJSON();
+		lastID++;
+		profile.id = lastID;
+		profiles.push(profile);	
+		res.status(201).send("Status: " + res.statusCode + " Added new user with id " + profile.id);
+	} else {
+		res.status(200);
+		res.send("Status: " +res.statusCode+ " Profile with email: " + profile.email + " already exists!");
+	};
+});
+
+// app.put('api/profiles', function(req, res){
+// 	// Update profile using email as identifier.
+// });
+
+// app.delete('api/profiles', function(req, res){
+// 	// Delete profile using email as identifier.
+// });
+
+
+// SERVER
 var server = app.listen(8000, function () {
 
   var host = server.address().address;
