62f007ef3837a32d4502795df66e5e3cea629a5b
Chris Oloff <christian.oloff@gmail.com>
2015-03-17 11:34:30 +0200

    work in progress

--- app.js
+++ app.js
@@ -30,13 +30,12 @@ var fixtures = [
 	}
 ];
 
-fixtures.forEach(function(value){
-	var now = new Date().toJSON();
+fixtures.forEach(function(profile){
 	lastID++;
 
-	value.date_joined = now;
-	value.id = lastID;
-	profiles.push(value);
+	profile.date_joined = new Date();
+	profile.id = lastID;
+	profiles.push(profile);
 });
 
 
@@ -45,6 +44,17 @@ app.get('/', function (req, res) {
   res.render('pages/index', {title: "Hi there, I'm an Express Server! Enter your details below."});
 });
 
+var validators = {
+  email: function(value) {
+    return validateEmail(value);
+  },
+  first_name: function(value) {
+    return value && value.length;
+  },
+  last_name: function(value) {
+    return value && value.length;
+  }
+}
 
 // PROFILES API
 // Find Profile from dummy DB with ID. Returns profile or null.
@@ -75,7 +85,7 @@ function validateEmail(email) {
 }
 
 function validateFirstLastName(first, last){
-	if (first.length>0 && last.length>0){
+	if (first && first.length > 0 && last && last.length > 0){
 		return true;
 	} else {
 		return false;
@@ -86,6 +96,7 @@ function validateFirstLastName(first, last){
 app.get('/api/profiles', function(req, res){
 	res.status(200).send(profiles);
 });
+
 // Profiles GET by ID
 app.get('/api/profiles/:id', function(req, res){
 	var response = findProfileByID(req.params.id);
@@ -96,47 +107,66 @@ app.get('/api/profiles/:id', function(req, res){
 			res.status(200).send(response);
 		};
 });
+
 // Profiles POST
 app.post('/api/profiles', function(req, res){
-	var profile = req.body;
-	var namesValid = validateFirstLastName(profile.first_name, profile.last_name);
-	var emailvalid = validateEmail(profile.email);
+	var body = req.body;
+	var namesValid = validateFirstLastName(body.first_name, body.last_name);
+	var emailValid = validateEmail(body.email);
 
-	if (emailvalid && emailvalid) {
-		var response = findProfileByEmail(profile.email);
+	if (emailValid && namesValid) {
+		var existingProfile = findProfileByEmail(body.email);
 		
-		if (response == null) {
-			var now = new Date().toJSON();
+		if (existingProfile == null) {
 			lastID++;
-			profile.id = lastID;
+      var profile = {
+        id: lastID,
+        date_joined: new Date(),
+        email: body.email,
+        first_name: body.first_name,
+        last_name: body.last_name
+      };
 			profiles.push(profile);
 			res.status(201).send("Status: " + res.statusCode + ", Added new user with id " + profile.id);
 		} else {
-			res.status(200);
-			res.send("Status: " +res.statusCode+ ", Profile with email: " + profile.email + " already exists!");
+			res.status(400);
+			res.send("Status: " + res.statusCode + ", Profile with email: " + profile.email + " already exists.");
 		};	
 	} else {
-		res.status(200).send("Status: " + res.statusCode + ", Please Enter a valid Email address.")
+		res.status(400).send("input validation failed");
 	};
 });
-// Profiles PUT
-app.put('/api/profiles', function(req, res){
-	var profile_update = req.body;
-	var profile = findProfileByEmail(profile_update.email);
-	var namesValid = validateFirstLastName(profile_update.first_name, profile_update.last_name)
 
-	if (profile && namesValid){
-		for (i = 0; i <= profiles.length; i++){
-			if (profiles[i].id == profile.id){
-				profile_update.id = profile.id;
-				profiles[i] = profile_update;
-				res.status(201).send("Status: "+res.statusCode+ ", Updated profile with ID: "+profile.id);
-			};
-		};
+app.put('/api/profiles/:id', function(req, res){
+	var profile_update = req.body;
+	var profile = findProfileByID(req.params.id);
+
+  var updateableProperties = [
+    'first_name', 'last_name', 'email'
+  ];
+
+	if (profile) {
+    var areAllUpdatesValid = true;
+    updateableProperties.forEach(function(property) {
+      var validator = validators[property];
+      var isValid = validator(profile_update[property]);
+      if (!isValid) {
+        areAllUpdatesValid = false;
+      }
+    });
+    if (areAllUpdatesValid) {
+      updateableProperties.forEach(function(property) {
+        profile[property] = profile_update[property];
+      });
+      res.status(200).send("Status: "+res.statusCode+ ", Updated profile with ID: "+profile.id);
+    } else {
+      res.status(400).send('Invalid update(s)');
+    }
 	} else {
-		res.status(200).send("Status: "+res.statusCode+", Could not update profile!");
+		res.status(404).send("Profile not found");
 	};
 });
+
 // Profiles DELETE
 app.delete('/api/profiles/:id', function(req, res){
 	var id = req.params.id;
